8b4ef9e820decffef0d6b34c2c0ae2194a58a936
Cesar Caceres <cesarc770@gmail.com>
2018-02-12 16:12:49 -0700

    modified client landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Landing = () => {
 	return (
 		<div style={{ textAlign: 'center'}}>
-			<h1>Emaily!</h1>
-			Collect feedback from your users
+			<h1>NYT Search</h1>
+			Search articles from New York Times
 		</div>
 
 	);
