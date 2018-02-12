6642f8a504c9c5d181adaae05ed99a6a2a9ee4be
Cesar Caceres <cesarc770@gmail.com>
2018-02-11 21:50:54 -0700

    adding server and client

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+	googleClientID: "893567915270-htmbrop7kkag7trg6cb7bn81cf4oc1cu.apps.googleusercontent.com",
+	googleClientSecret: "uaV2oWNwdRwFqByJWSZ30G3_",
+	mongoURI: 'mongodb://admin:password@ds135757.mlab.com:35757/emaily-dev1',
+	cookieKey: 'ihsfjhsdfsdjhfsjdhfjshdfjkshdfksskdhfjksd'
+}
